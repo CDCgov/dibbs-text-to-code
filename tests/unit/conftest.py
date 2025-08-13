e1ff7ab09a570f18c0a8fe426f577d3ed506371b
Marcelle <53578688+m-goggins@users.noreply.github.com>
2025-08-13 13:51:34 -0700

    Add function to retrieve manifest data (#44)
    ## Description
    
    Based on our current understanding of how TTC will integrate with APHL's
    setup, we think that the APHL bridge will send an event to the TTC
    lambda function when an eICR needs to be standardized. This PR adds the
    functionality for the TTC lambda function to accept an EventBridge event
    and return the files listed in the record(s).
    
    ## Related Issues
    Closes #36
    
    ## Additional Notes
    I used `moto` to mock the AWS functionality for our tests.

---
+++ tests/unit/conftest.py
@@ -0,0 +1,28 @@
+import os
+
+import boto3
+import moto
+import pytest
+
+
+@pytest.fixture(scope="function")
+def moto_setup(monkeypatch):
+    with moto.mock_aws():
+        monkeypatch.setenv("AWS_REGION", "us-east-1")
+        monkeypatch.setenv("AWS_ACCESS_KEY_ID", "test")
+        monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "test")
+        bucket_name = "test-bucket"
+
+        # Create the fake S3 bucket
+        s3 = boto3.client(
+            "s3",
+            region_name=os.environ["AWS_REGION"],
+            aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+            aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+        )
+        s3.create_bucket(Bucket=bucket_name)
+
+        # Add convenience attribute for tests
+        s3.bucket_name = bucket_name
+
+        yield s3
