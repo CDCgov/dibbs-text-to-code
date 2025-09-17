0775b6972636d4354dac29e0a14102f8a877ee34
m-goggins <marcelle.goggins@gmail.com>
2025-09-17 13:55:56 -0400

    merge lists and enhancements

--- tests/unit/test_normalize.py
+++ tests/unit/test_normalize.py
@@ -1,16 +1,134 @@
+import pytest
+
 from utils import normalize as utils
 
 
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        # Removes special characters and extra spaces, converts to lowercase
+        (
+            " Cell growth [Presence] of Amniocytes Qualitative by Tissue culture",
+            "cell growth presence of amniocytes qualitative by tissue culture",
+        ),
+        # Replaces special characters with spaces
+        (
+            "Power spectrum.theta frequency/Power spectrum.total",
+            "power spectrum theta frequency power spectrum total",
+        ),
+        (
+            "Platelet aggregation.ristocetin induced^125 ug/mL",
+            "platelet aggregation ristocetin induced 125 ug ml",
+        ),
+    ],
+)
 class TestNormalizeText:
-    def test_normalize_text(self):
-        text = " Cell growth [Presence] of Amniocytes Qualitative by Tissue culture"
-        assert (
-            utils.normalize_text(text)
-            == "cell growth presence of amniocytes qualitative by tissue culture"
-        )
+    def test_normalize_text(self, text, expected):
+        assert utils.normalize_text(text) == expected
 
-        text = "Power spectrum.theta frequency/Power spectrum.total"
-        assert utils.normalize_text(text) == "power spectrum theta frequency power spectrum total"
 
-        text = "VFr.DF"
-        assert utils.normalize_text(text) == "vfr df"
+@pytest.mark.parametrize(
+    "existing, new, expected",
+    [
+        # Merges two lists with some overlap, preserving order and uniqueness
+        (["a", "b", "c"], ["b", "c", "d"], ["a", "b", "c", "d"]),
+        # Merges two lists with no overlap
+        (["a", "b", "c"], ["d", "e", "f"], ["a", "b", "c", "d", "e", "f"]),
+        # Merges when one list is empty
+        ([], ["a", "b", "c"], ["a", "b", "c"]),
+        # Merges two identical lists
+        (["a", "b", "c"], ["a", "b", "c"], ["a", "b", "c"]),
+    ],
+)
+class TestMergeTwoLists:
+    def test_merge_two_lists(self, existing, new, expected):
+        merged = utils.merge_two_lists(existing, new)
+        assert merged == expected
+
+
+@pytest.mark.parametrize(
+    "dict1, dict2, expected",
+    [
+        # Merges two dicts with some overlap, preserving order and uniqueness
+        (
+            {
+                "EntSub": {"code": "LP100323-7", "abbr": [], "replacement": ["Entitic substance"]},
+                "ClockTime": {"code": "LP101588-4", "abbr": ["Clock time"], "replacement": []},
+                "EngRatFr": {"code": "LP101814-4", "abbr": [], "replacement": ["hello"]},
+            },
+            {
+                "EngRatFr": {"code": "LP101814-4", "abbr": [], "replacement": ["hello", "goodbye"]},
+                "PPresDiff": {"code": "LP101909-2", "abbr": [], "replacement": []},
+                "VFrDiff": {"code": "LP101984-5", "abbr": [], "replacement": []},
+            },
+            {
+                "EntSub": {"code": "LP100323-7", "abbr": [], "replacement": ["Entitic substance"]},
+                "ClockTime": {"code": "LP101588-4", "abbr": ["Clock time"], "replacement": []},
+                "EngRatFr": {"code": "LP101814-4", "abbr": [], "replacement": ["hello", "goodbye"]},
+                "PPresDiff": {"code": "LP101909-2", "abbr": [], "replacement": []},
+                "VFrDiff": {"code": "LP101984-5", "abbr": [], "replacement": []},
+            },
+        ),
+        # Merges two dicts with no overlap
+        (
+            {
+                "EntSub": {"code": "LP100323-7", "abbr": [], "replacement": ["Entitic substance"]},
+                "ClockTime": {"code": "LP101588-4", "abbr": ["Clock time"], "replacement": []},
+                "EngRatFr": {"code": "LP101814-4", "abbr": [], "replacement": ["hello"]},
+            },
+            {
+                "TscoreDiff": {
+                    "code": "LP202986-8",
+                    "abbr": [],
+                    "replacement": ["T-score", "Score difference"],
+                },
+            },
+            {
+                "EntSub": {"code": "LP100323-7", "abbr": [], "replacement": ["Entitic substance"]},
+                "ClockTime": {"code": "LP101588-4", "abbr": ["Clock time"], "replacement": []},
+                "EngRatFr": {"code": "LP101814-4", "abbr": [], "replacement": ["hello"]},
+                "TscoreDiff": {
+                    "code": "LP202986-8",
+                    "abbr": [],
+                    "replacement": ["T-score", "Score difference"],
+                },
+            },
+        ),
+        # Merges when one dict is empty
+        (
+            {},
+            {
+                "EntSub": {"code": "LP100323-7", "abbr": [], "replacement": ["Entitic substance"]},
+                "ClockTime": {"code": "LP101588-4", "abbr": ["Clock time"], "replacement": []},
+                "EngRatFr": {"code": "LP101814-4", "abbr": [], "replacement": ["hello"]},
+            },
+            {
+                "EntSub": {"code": "LP100323-7", "abbr": [], "replacement": ["Entitic substance"]},
+                "ClockTime": {"code": "LP101588-4", "abbr": ["Clock time"], "replacement": []},
+                "EngRatFr": {"code": "LP101814-4", "abbr": [], "replacement": ["hello"]},
+            },
+        ),
+        # Merges two identical dicts
+        (
+            {
+                "EntSub": {"code": "LP100323-7", "abbr": [], "replacement": ["Entitic substance"]},
+                "ClockTime": {"code": "LP101588-4", "abbr": ["Clock time"], "replacement": []},
+                "EngRatFr": {"code": "LP101814-4", "abbr": [], "replacement": ["hello"]},
+            },
+            {
+                "EntSub": {"code": "LP100323-7", "abbr": [], "replacement": ["Entitic substance"]},
+                "ClockTime": {"code": "LP101588-4", "abbr": ["Clock time"], "replacement": []},
+                "EngRatFr": {"code": "LP101814-4", "abbr": [], "replacement": ["hello"]},
+            },
+            {
+                "EntSub": {"code": "LP100323-7", "abbr": [], "replacement": ["Entitic substance"]},
+                "ClockTime": {"code": "LP101588-4", "abbr": ["Clock time"], "replacement": []},
+                "EngRatFr": {"code": "LP101814-4", "abbr": [], "replacement": ["hello"]},
+            },
+        ),
+    ],
+)
+class TestMergeEnhancements:
+    def test_merge_enhancements(self, dict1, dict2, expected):
+        merged = utils.merge_enhancements(dict1, dict2)
+        assert merged == expected
--- utils/normalize.py
+++ utils/normalize.py
@@ -1,3 +1,5 @@
+import typing
+
 import utils.regex_patterns as rp
 
 
@@ -5,7 +7,61 @@ def normalize_text(text: str) -> str:
     """
     Normalize text for comparison by removing non-alphanumeric characters,
     converting to lowercase, and removing all trailing, leading, and excess whitespace.
+    :param text: The input text to normalize.
+    :return: The normalized text.
     """
     text = rp.ALPHA_NUMERIC.sub(" ", text)
 
     return " ".join(text.strip().lower().split())
+
+
+# TODO: Add pydantic models for type checking
+def merge_enhancements(
+    *dicts: typing.Dict[str, typing.Dict[str, typing.Any]],
+) -> typing.Dict[str, typing.Dict[str, typing.Any]]:
+    """
+     Merge multiple typing.Dictionaries of LOINC enhancements into a single typing.Dictionary.
+    Merges 'abbr' and 'replacement' lists, preserves order and uniqueness,
+    keeps the first-seen 'code' for each key.
+    :param typing.Dicts: Variable number of typing.Dictionaries to merge.
+    :return: A single typing.Dictionary with merged enhancements.
+    """
+
+    merged: typing.Dict[str, typing.Dict[str, typing.Any]] = {}
+
+    for d in dicts:
+        for key, value in d.items():
+            code = value["code"]
+            abbrs = value.get("abbr", [])
+            replacements = value.get("replacement", [])
+
+            if key not in merged:
+                merged[key] = {
+                    "code": code,
+                    "abbr": [],
+                    "replacement": [],
+                }
+            # Keep first-seen code
+            if merged[key]["code"] is None and code is not None:
+                merged[key]["code"] = code
+
+            # Merge and deduplicate abbrs while preserving order
+            merged[key]["abbr"] = merge_two_lists(merged[key]["abbr"], abbrs)
+
+            # Merge and deduplicate replacements while preserving order
+            merged[key]["replacement"] = merge_two_lists(merged[key]["replacement"], replacements)
+
+    return merged
+
+
+def merge_two_lists(
+    existing: typing.List[typing.Any], new: typing.List[typing.Any]
+) -> typing.List[typing.Any]:
+    """
+    Merge two lists while preserving order and uniqueness.
+    :param list1: The first list.
+    :param list2: The second list.
+    :return: A merged list with unique elements in order of first appearance.
+    """
+
+    return existing + [v for v in new if v not in existing]
