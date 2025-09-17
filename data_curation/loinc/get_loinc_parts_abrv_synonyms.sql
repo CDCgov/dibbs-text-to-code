25a61e330535962a6f37b8cc69eaacc4d08a6038
Brady Fausett <brady.fausett@skylight.digital>
2025-09-17 08:53:14 -0600

    Create script to generate LOINC Part Enhancement Files - Dictionaries per LOINC Part Axis containing the Part and it's abbreviations & synonyms (#80)
    ## Description
    
    - SQL files to be used to query the RELMA DB to generate the same |
    delimited files in the future
    - Left comments in the file to describe process of what to do to get
    this file generated
    - The output from RELMA DB containing the LOINC Part Axis data
    - Script to process LOINC Part Axis file into LOINC Part Axis
    Dictionaries
    - The actual Dictionary Files themselves for each of the Loinc parts has
    been generated in the proper folder
    
    ## Related Issues
    
    Closes #76 
    
    ## Additional Notes
    
    <--------------------- REMOVE THE LINES BELOW BEFORE MERGING
    --------------------->
    
    ## Checklist
    
    Please review and complete the following checklist before submitting
    your pull request:
    
    - [ ] I have ensured that the pull request is of a manageable size,
    allowing it to be reviewed within a single session.
    - [ ] I have reviewed my changes to ensure they are clear, concise, and
    well-documented.
    - [ ] I have updated the documentation, if applicable.
    - [ ] I have added or updated test cases to cover my changes, if
    applicable.
    - [ ] I have minimized the number of reviewers to include only those
    essential for the review.
    
    ## Checklist for Reviewers
    
    Please review and complete the following checklist during the review
    process:
    
    - [ ] The code follows best practices and conventions.
    - [ ] The changes implement the desired functionality or fix the
    reported issue.
    - [ ] The tests cover the new changes and pass successfully.
    - [ ] Any potential edge cases or error scenarios have been considered.

---
+++ data_curation/loinc/get_loinc_parts_abrv_synonyms.sql
@@ -0,0 +1,14 @@
+SELECT PART_TYPE_NAME.NAME, PART.PART_NUM, PART.PART, PART.NAME, PART.DESCRIPTION, IIf([PART]![PREF_ABRV]<>[PART]![PART],[PART]![PREF_ABRV],"") AS PREF_ABRV, IIf([SYNONYM_TBL]![SYNONYMS]<>[PART]![PART],IIf([SYNONYM_TBL]![SYNONYMS]<>[PART]![PREF_ABRV],[SYNONYM_TBL]![SYNONYMS],""),IIf([SYNONYM_TBL]![SYNONYMS]<>[PART]![PREF_ABRV],[SYNONYM_TBL]![SYNONYMS],"")) AS SYNONYM INTO LOINC_PARTS_ABRV_SYNONYMS
+FROM ((PART_TYPE_NAME LEFT JOIN PART ON PART_TYPE_NAME.PART_TYPE = PART.TYPE) LEFT JOIN PART_SYNONYM_LK ON PART.PART_NUM = PART_SYNONYM_LK.PART_NUMBER) LEFT JOIN SYNONYM_TBL ON PART_SYNONYM_LK.SYNONYM_NUMBER = SYNONYM_TBL.SYNONYM_NUM
+WHERE (((PART.PART) Not Like "[{]*" And (PART.PART) Not Like "[*]*" And (PART.PART) Not Like "[?]*" And (PART.PART) Not Like "[-]*" And (PART.PART) Not Like "[_]*") AND ((PART_TYPE_NAME.PART_TYPE) In (1,2,3,4,5,6)))
+ORDER BY PART_TYPE_NAME.NAME, PART.PART_NUM;
+
+-- Note: after creating the above query in the RELMA Access DB, you will 
+-- want to save the query as a table and then you can export the table as a text file with | delimiters
+-- ensure the name of the table is LOINC_PARTS_ABBRV_SYNONYMS
+
+-- NOTE: you will have to manually 'find' and remove special characters (ie.  20666|"COMPONENT"|"LP17035-4"|"Allium cepa"|||"Onion"|"Allium salota Dost�l")
+-- So far only 1 special character was found (see example above)
+
+-- Save the exported file into the /data_curation/loinc folder, at the base of this repo, with a 
+-- file name of: LOINC_PARTS_ABBRV_SYNONYMS.txt
