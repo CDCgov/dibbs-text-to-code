d8ffd46aafe67f3678fca610e5fb2999a3b6741b
m-goggins <marcelle.goggins@gmail.com>
2025-10-06 13:19:48 -0400

    cleanup part 2

--- model_tuning/performance.py
+++ model_tuning/performance.py
@@ -7,7 +7,7 @@ from sentence_transformers import SentenceTransformer
 from sentence_transformers import util
 from torch import Tensor
 
-MODEL_NAME = "all-MiniLM-L6-v2d"
+MODEL_NAME = "all-MiniLM-L6-v2"
 SNOINC_CODES_FILE = "../data/snoinc_extracts/loinc_lab_names_20250911.csv"
 # use date in filename to keep track of versions
 DATE = SNOINC_CODES_FILE.split("_")[-1].split(".")[0]
