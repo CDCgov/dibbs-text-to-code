ceeddb440f541c7c4666a08729862646b99fd68a
Josh Nygaard <josh.nygaard@gmail.com>
2025-10-31 13:20:14 -0400

    remove feedback

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router';
 import DibbsLogo from '../../assets/dibbs-logo.svg';
 import CdcLogo from '../../assets/cdc-logo.svg';
-import { ProvideFeedbackButton } from '../ProvideFeedbackButton';
 
 import NavigationBar from '../NavigationBar';
 import { Icon } from '@trussworks/react-uswds';
@@ -25,7 +24,6 @@ export function Layout({ children }: LayoutProps) {
       >
         {children}
       </main>
-      <ProvideFeedbackButton />
       <Footer />
     </div>
   );
