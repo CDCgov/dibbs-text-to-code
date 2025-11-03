01fdb4f116c4089d1967f745a92f76088fcf2233
Josh Nygaard <josh.nygaard@gmail.com>
2025-11-03 15:04:43 -0500

    some more tests

--- frontend/src/components/DescriptionLine/index.test.tsx
+++ frontend/src/components/DescriptionLine/index.test.tsx
@@ -1,19 +1,23 @@
-import { render } from "@testing-library/react"
-import { DescriptionLine } from "."
+import { render } from '@testing-library/react';
+import { DescriptionLine } from '.';
 
 describe('DescriptionLine', () => {
   let container: HTMLElement;
+  const testName = 'Test Name';
+  const testDetails = 'Test Details';
 
   beforeEach(() => {
-    const result = render(<DescriptionLine name="Test Name" details="Test Details"/>);
+    const result = render(
+      <DescriptionLine name={testName} details={testDetails} />
+    );
     container = result.container;
   });
 
-  it("Should display the given name", () => {
-    expect(container.textContent).toContain('Test Name');
+  it('Should display the given name', () => {
+    expect(container.textContent).toContain(testName);
   });
 
-  it("Should display the given details", () => {
-    expect(container.textContent).toContain('Test Details');
+  it('Should display the given details', () => {
+    expect(container.textContent).toContain(testDetails);
   });
 });
