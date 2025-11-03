7fdcabbad48ca26bc12da72040d2319dc9fad5f2
Josh Nygaard <josh.nygaard@gmail.com>
2025-11-03 14:51:42 -0500

    improve description line tests

--- frontend/src/components/DescriptionLine/index.test.tsx
+++ frontend/src/components/DescriptionLine/index.test.tsx
@@ -2,13 +2,18 @@ import { render } from "@testing-library/react"
 import { DescriptionLine } from "."
 
 describe('DescriptionLine', () => {
+  let container: HTMLElement;
+
+  beforeEach(() => {
+    const result = render(<DescriptionLine name="Test Name" details="Test Details"/>);
+    container = result.container;
+  });
+
   it("Should display the given name", () => {
-    const {container} = render(<DescriptionLine name="Test Name" details="_"/>)
     expect(container.textContent).toContain('Test Name');
   });
 
   it("Should display the given details", () => {
-    const {container} = render(<DescriptionLine name="_" details="Test Details"/>)
     expect(container.textContent).toContain('Test Details');
   });
 });
