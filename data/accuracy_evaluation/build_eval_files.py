b2778f79fd499dd1563cdce33520de9ddf296804
Rob Mitchell <robert.a.mitchell@skylight.digital>
2025-12-04 10:22:43 -0500

    updating documentation, refactoring to not need pandas

--- data/accuracy_evaluation/build_eval_files.py
+++ data/accuracy_evaluation/build_eval_files.py
@@ -1,7 +1,6 @@
 import json
 import os
 
-import pandas as pd
 import requests
 from dotenv import load_dotenv
 
@@ -11,16 +10,16 @@ load_dotenv()
 # TES API Key can be obtained at https://tes.tools.aimsplatform.org/
 TES_API_KEY = os.environ.get("TES_API_KEY")
 TES_BASE_URL = "https://tes.tools.aimsplatform.org/api/fhir/ValueSet/"
-TES_ENDPOINTS = ["lrtc", "lotc"]  # Endpoints for LOINC-related value sets
-
 headers = {"x-api-key": TES_API_KEY, "Accept": "application/fhir+json"}
-
 
 # ERSD, key can be obtained at https://ersd.aimsplatform.org/#/api-keys
 ERSD_API_KEY = os.environ.get("ERSD_API_KEY")
 ERSD_BASE_URL = (
     f"https://ersd.aimsplatform.org/api/ersd/v3specification?format=json&api-key={ERSD_API_KEY}"
 )
+
+# Endpoints for LOINC-related value sets in TES and ERSD
+VSTYPE_ENDPOINTS = ["lrtc", "lotc"]
 
 
 def get_ersd_valuesets(endpoints: list) -> tuple[dict, list]:
@@ -39,17 +38,19 @@ def get_ersd_valuesets(endpoints: list) -> tuple[dict, list]:
     return response, oids
 
 
-def get_tes_valuesets(endpoints: list) -> set:
+def get_tes_valuesets(endpoints: list) -> list:
     """
     Fetches LOINC value sets from TES API endpoints.
     :param endpoints: List of TES API endpoints to fetch value sets from.
     """
-    oids = set()
+    oids = []
     for endpoint in endpoints:
         url = f"{TES_BASE_URL}{endpoint}"
         response = requests.get(url, headers=headers)
-        for oid in response.json().get("compose").get("include"):
-            oids.add(oid.get("valueSet")[0].split("/")[-1])
+        for resp in response.json().get("compose").get("include"):
+            oid = resp.get("valueSet")[0].split("/")[-1]
+            if oid not in oids:
+                oids.append(oid)
     return oids
 
 
@@ -85,9 +86,10 @@ def build_ersd_mapping_files(response: dict, oids: list) -> tuple[dict, dict]:
     return oid_to_conditions, loinc_to_oids
 
 
-def build_tes_mapping_files(oids: set) -> tuple[dict, dict]:
+def build_tes_mapping_files(oids: list) -> tuple[dict, dict]:
     """
     Builds mapping files for OIDs to conditions and LOINC codes to OIDs.
+    TODO: Potentially refactor to build using rs-grouper
     :param oids: Set of OIDs to build mappings for.
     """
     oid_to_conditions = {}
@@ -122,24 +124,28 @@ def evaluate_mapping_files(oid_to_conditions: dict, loinc_to_oids: dict):
     :param oid_to_conditions: OID to conditions mapping.
     :param loinc_to_oids: LOINC to OIDs mapping.
     """
-    df = pd.DataFrame({"loinc": list(loinc_to_oids.keys()), "oids": list(loinc_to_oids.values())})
-    n = len(df)
+    n = len(loinc_to_oids)
     print("Total LOINC codes mapped:", n)
 
-    df["oid_count"] = df["oids"].apply(len)
-    print("LOINC codes mapped to multiple OIDs:", len(df[df["oid_count"] > 1]) / n * 100, "%")
-    print(df["oid_count"].value_counts().sort_index())
+    multiple_oids = {}
+    for oids in loinc_to_oids.values():
+        k = len(oids)
+        multiple_oids[k] = multiple_oids.get(k, 0) + 1
+    pct_multi_oids = (n - multiple_oids.get(1)) / n * 100
+    print("LOINC codes mapped to multiple OIDs:", pct_multi_oids, "%")
+    for k in sorted(multiple_oids):
+        print(k, multiple_oids[k])
 
-    df["conditions"] = df["oids"].apply(
-        lambda oid_list: [oid_to_conditions.get(oid) for oid in oid_list]
-    )
-    df["unique_condition_count"] = df["conditions"].apply(lambda x: len(set(x)))
-    print(
-        "LOINC codes mapped to multiple unique conditions:",
-        len(df[df["unique_condition_count"] > 1]) / n * 100,
-        "%",
-    )
-    print(df.unique_condition_count.value_counts().sort_index())
+    multiple_conditions = {}
+    for oids in loinc_to_oids.values():
+        conditions = [oid_to_conditions.get(oid) for oid in oids]
+        unique_count = len(set(conditions))
+        multiple_conditions[unique_count] = multiple_conditions.get(unique_count, 0) + 1
+
+    pct_multi_conditions = (n - multiple_conditions.get(1)) / n * 100
+    print("LOINC codes mapped to multiple unique conditions:", pct_multi_conditions, "%")
+    for k in sorted(multiple_conditions):
+        print(k, multiple_conditions[k])
 
 
 def export_mapping_files(mapping: dict, filename: str):
@@ -154,13 +160,17 @@ def export_mapping_files(mapping: dict, filename: str):
 
 if __name__ == "__main__":
     # Build ERSD mapping files
-    response, oids = get_ersd_valuesets(TES_ENDPOINTS)
+    response, oids = get_ersd_valuesets(VSTYPE_ENDPOINTS)
     oid_to_conditions, loinc_to_oids = build_ersd_mapping_files(response, oids)
 
     # Build TES mapping files
-    # oids = get_tes_valuesets(TES_ENDPOINTS)
-    # tes_oid_to_conditions, tes_loinc_to_oids = build_tes_mapping_files(oids)
+    # tes_oids = get_tes_valuesets(VSTYPE_ENDPOINTS)
+    # tes_oid_to_conditions, tes_loinc_to_oids = build_tes_mapping_files(tes_oids)
 
     evaluate_mapping_files(oid_to_conditions, loinc_to_oids)
     export_mapping_files(oid_to_conditions, "data/accuracy_evaluation/oid_to_conditions.txt")
     export_mapping_files(loinc_to_oids, "data/accuracy_evaluation/loinc_to_oids.txt")
+
+    # evaluate_mapping_files(tes_oid_to_conditions, tes_loinc_to_oids)
+    # export_mapping_files(tes_loinc_to_oids, "data/accuracy_evaluation/tes_loinc_to_oids.txt")
+    # export_mapping_files(tes_oid_to_conditions, "data/accuracy_evaluation/tes_oid_to_conditions.txt")
