3968cc0848013faa74a84ad3eeb677657ad8d39a
Brady Fausett <brady.fausett@skylight.digital>
2025-12-15 08:17:45 -0700

    fix broken tests

--- src/dibbs_text_to_code/services/text_processor.py
+++ src/dibbs_text_to_code/services/text_processor.py
@@ -11,7 +11,7 @@ def _get_sentence_transformer():
 
 
 def embed(input_text: str) -> Tensor:
-    """Takes a text string and embeds it as vectors
+    """Takes a text string and embeds it as vectorspip
     using a model as defined in config.py.
 
     :param input_text: Text string to embed.
