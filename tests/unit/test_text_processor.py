3402c4311d3f696ec2782f0d0e8a8a2002017153
Brady Fausett <brady.fausett@skylight.digital>
2025-12-23 10:41:42 -0700

    Extract Schematron Errors for TTC data fields of interest from APHL Schematron Output (#189)
    ## Description
    
    Added a basic function to accept schematron output, from APHL, and parse
    through to find 'errors' of interest to TTC. These are stored in a list,
    by data field type, in our Configs. These error texts 'SHOULD' be
    confirmed with APHL or this step will fail. If we can find a better way
    to find errors of interest we should use it, but so far, based upon the
    output provided by APHL, this seems to be the route.
    
    Also, note that we will need to verify that the XPATHs provided will be
    useful and work for us in the later steps. I'll attempt to test this in
    the next stories. If there are issues, I will adjust this function to
    adapt the XPATHs to what we need.
    
    ## Related Issues
    
    Closes #171 
    
    ## Additional Notes
    
    Note that we can modify how we are doing this, but we have something in
    place for now.
    
    ## Checklist for Reviewers
    
    Please review and complete the following checklist during the review
    process:
    
    - [ ] The code follows best practices and conventions.
    - [ ] The changes implement the desired functionality or fix the
    reported issue.
    - [ ] The tests cover the new changes and pass successfully.
    - [ ] Any potential edge cases or error scenarios have been considered.

--- tests/unit/test_text_processor.py
+++ tests/unit/test_text_processor.py
@@ -1,7 +1,18 @@
+from pathlib import Path
+
 from dibbs_text_to_code.services import text_processor
+
+current_dir = Path(__file__).parent
 
 
 class TestTextProcessor:  # noqa: D101
+    def setup_files(self):
+        schematron_path = current_dir / "assets" / "test_schematron_errors.xml"
+        with open(schematron_path, "r", encoding="utf-8") as f:
+            schematron_output = f.read()
+            print(len(schematron_output))
+        return schematron_output
+
     def test_is_text_viable_wrong_field(self):
         data_field = "LABs"
         text_value = "Here is my test"
@@ -71,3 +82,19 @@ class TestTextProcessor:  # noqa: D101
         # this is only for the small model - 384
         # this is only for the Qwen model - 4096  # number of dimensions
         assert len(embedding.shape) == 1  # Assuming a 1D tensor for a single string
+
+    def test_get_schematron_error_data_fields(self):
+        schematron_errors = self.setup_files()
+        result = text_processor.get_data_fields_from_schematron_error(schematron_errors)
+
+        assert result != {}
+        assert "lab_result" in result
+        assert len(result["lab_result"]) == 2
+        assert "lab_order" in result
+        assert len(result["lab_order"]) == 1
+
+    def test_get_schematron_error_empty_xml(self):
+        schematron_errors = ""
+        result = text_processor.get_data_fields_from_schematron_error(schematron_errors)
+
+        assert result == {}
