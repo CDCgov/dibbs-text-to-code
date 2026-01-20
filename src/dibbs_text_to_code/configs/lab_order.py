822ea02880eb8780537325753e4e8b43f45cb41e
Josh Nygaard <jnygaard@skylight.digital>
2026-01-20 13:20:27 -0500

    code does not have text

--- src/dibbs_text_to_code/configs/lab_order.py
+++ src/dibbs_text_to_code/configs/lab_order.py
@@ -13,7 +13,6 @@ class LabOrderConfig(pydantic.BaseModel):
     xpaths: list[str] = [
         "/code/@displayName",
         "/code/originalText",
-        "/code/text",
         "/code/translation/@displayName",
         "/code/translation/originalText",
         "/code/translation/text",
