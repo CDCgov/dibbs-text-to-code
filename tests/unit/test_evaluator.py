be840f25b74ee00a4a79969b620d8628cf43e67a
m-goggins <marcelle.goggins@gmail.com>
2026-01-20 15:54:16 -0500

    add back test and remove Nones

--- tests/unit/test_evaluator.py
+++ tests/unit/test_evaluator.py
@@ -1,17 +1,19 @@
+import pytest
+
 from dibbs_text_to_code.schemas import eicr
 from dibbs_text_to_code.schemas import registry
 from dibbs_text_to_code.services import evaluator
 
 
 class TestEvaluator:
-    def test_set_sentence_transformer(self) -> None:
+    def test_set_sentence_transformer(self):
         assert evaluator._model is None
 
         evaluator._set_sentence_transformer(registry._model_name)
 
         assert isinstance(evaluator._model, evaluator.SentenceTransformer)
 
-    def test_meets_word_count(self) -> None:
+    def test_meets_word_count(self):
         text_value = "This is a simple test string"
         word_count = 4
         expected_result = True
@@ -23,14 +25,14 @@ class TestEvaluator:
 
         assert evaluator._meets_word_count(text_value, word_count) == expected_result
 
-    # def test_is_text_viable_wrong_field(self) -> None:
-    #     data_field = "LABs"
-    #     text_value = "Here is my test"
+    def test_is_text_viable_wrong_field(self):
+        data_field = "LABs"
+        text_value = "Here is my test"
 
-    #     with pytest.raises(KeyError):
-    #         evaluator.is_text_viable(data_field, text_value)
+        with pytest.raises(KeyError):
+            evaluator.is_text_viable(data_field, text_value)
 
-    def test_is_text_viable_empty_txt(self) -> None:
+    def test_is_text_viable_empty_txt(self):
         data_field = eicr.EicrDataField.LAB_TEST_NAME_ORDERED
         text_value = ""
         expected_result = False
@@ -42,21 +44,21 @@ class TestEvaluator:
 
         assert evaluator.is_text_viable(data_field, text_value) == expected_result
 
-    def test_is_text_viable_lab_order_viable(self) -> None:
+    def test_is_text_viable_lab_order_viable(self):
         data_field = eicr.EicrDataField.LAB_TEST_NAME_ORDERED
         text_value = "COVID PCR TEST FROM NASAL SWAB"
         expected_result = True
 
         assert evaluator.is_text_viable(data_field, text_value) == expected_result
 
-    def test_is_text_viable_lab_order_not_viable(self) -> None:
+    def test_is_text_viable_lab_order_not_viable(self):
         data_field = eicr.EicrDataField.LAB_TEST_NAME_ORDERED
         text_value = "COVID PCR"
         expected_result = False
 
         assert evaluator.is_text_viable(data_field, text_value) == expected_result
 
-    def test_embed(self) -> None:
+    def test_embed(self):
         input_text = "Influenza virus A and B and SARS-CoV-2 (COVID-19)"
         embedding = evaluator.embed(input_text)
 
