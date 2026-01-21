36cd8db1eca5fb85649603ab5a26cbf9ea18969f
Josh Nygaard <jnygaard@skylight.digital>
2026-01-21 10:53:10 -0500

    remove string constraints

--- src/dibbs_text_to_code/models.py
+++ src/dibbs_text_to_code/models.py
@@ -1,11 +1,8 @@
-from typing import Annotated
-
 from pydantic import BaseModel
-from pydantic.types import StringConstraints
 
 
 class Candidate(BaseModel):
     """Model representing a piece of text to be considered for encoding."""
 
-    value: Annotated[str, StringConstraints(min_length=3)]
+    value: str
     xpath: str
