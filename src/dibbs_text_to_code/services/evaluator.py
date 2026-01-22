2f123b253157c2055fb60886b2aaf48556f74227
Josh Nygaard <jnygaard@skylight.digital>
2026-01-22 09:21:49 -0500

    wip

--- src/dibbs_text_to_code/services/evaluator.py
+++ src/dibbs_text_to_code/services/evaluator.py
@@ -17,19 +17,14 @@ class Evaluator:
         """
         self._model_name = model_name
 
-    def _set_sentence_transformer(self) -> None:
-        """Set the SentenceTransformer model to be used for embedding text."""
-        if self._model is None:
-            self._model = SentenceTransformer(self._model_name)
-
     def embed(self, input_text: str) -> Tensor:
         """Take a text string and embeds it as a vector using a model as defined in config.py.
 
         :param input_text: Text string to embed.
         :returns: Tensor representation of input text.
         """
-        self._set_sentence_transformer()
-
+        if self._model is None:
+            self._model = SentenceTransformer(self._model_name)
         return self._model.encode(input_text)
 
 
