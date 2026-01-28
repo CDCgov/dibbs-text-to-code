3d0e0f8d8825d76c505f6edc96e855b9f39f80a7
Josh Nygaard <jnygaard@skylight.digital>
2026-01-28 11:16:06 -0500

    move model out of initialization

--- src/dibbs_text_to_code/services/embedder.py
+++ src/dibbs_text_to_code/services/embedder.py
@@ -3,25 +3,16 @@ from torch import Tensor
 
 from dibbs_text_to_code.models.registry import default_model
 
+_MODEL = SentenceTransformer(default_model)
+
 
 class Embedder:
     """Transforms nonstandard text."""
 
-    _model: SentenceTransformer | None = None
-
-    def __init__(self, model_name: str = default_model):
-        """Initialize embedder.
-
-        :model_name: Model name string.
-        """
-        self._model_name = model_name
-
-    def embed(self, input_text: str) -> Tensor:
+    def embed(self, text: str) -> Tensor:
         """Take a text string and embeds it as a vector using a model as defined in config.py.
 
-        :param input_text: Text string to embed.
+        :param text: Text string to embed.
         :returns: Tensor representation of input text.
         """
-        if self._model is None:
-            self._model = SentenceTransformer(self._model_name)
-        return self._model.encode(input_text)
+        return _MODEL.encode(text)
--- tests/unit/test_embedder.py
+++ tests/unit/test_embedder.py
@@ -17,6 +17,4 @@ class TestEmbedder:
         expected_embedding_length = 1024
 
         assert len(embedding) == expected_embedding_length
-        # this is only for the small model - 384
-        # this is only for the Qwen model - 4096  # number of dimensions
         assert len(embedding.shape) == 1  # Assuming a 1D tensor for a single string
