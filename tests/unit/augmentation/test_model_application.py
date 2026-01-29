065c58a47eece212beb5253fb31a625840903fb6
Brady Fausett <brady.fausett@skylight.digital>
2026-01-29 09:54:01 -0700

    fixes for tests

--- tests/unit/augmentation/test_model_application.py
+++ tests/unit/augmentation/test_model_application.py
@@ -2,7 +2,7 @@ from augmentation.models import ApplicationCode
 
 
 class TestApplicationModel:
-    def test_applicationcode(self):
+    def test_application_code(self):
         """Smoke tests for ApplicationCode enum."""
         app_enum = ApplicationCode
         assert app_enum.TEXT_TO_CODE.value == "text-to-code"
