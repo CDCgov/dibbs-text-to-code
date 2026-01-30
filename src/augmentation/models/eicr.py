d31989b3aa6b23d62a7120b3cc4ca44dd46e191c
Brady Fausett <brady.fausett@skylight.digital>
2026-01-30 08:42:21 -0700

    Update src/augmentation/models/eicr.py
    Co-authored-by: Marcelle <53578688+m-goggins@users.noreply.github.com>

--- src/augmentation/models/eicr.py
+++ src/augmentation/models/eicr.py
@@ -2,7 +2,7 @@ from enum import StrEnum
 
 
 class ECRDataField(StrEnum):
-    """Enum for eICR data fields relevant to the TTC module."""
+    """Enum for eICR data fields augmentation can modify."""
 
     LAB_TEST_NAME_RESULTED = "Lab Test Name Resulted"
     LAB_TEST_NAME_ORDERED = "Lab Test Name Ordered"
