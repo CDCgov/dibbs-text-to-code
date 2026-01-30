a701145f47cba24c5181073165e58ec1d91260d1
Brady Fausett <brady.fausett@skylight.digital>
2026-01-30 09:10:09 -0700

    Update test_model_application.py

--- tests/unit/augmentor/test_model_application.py
+++ tests/unit/augmentor/test_model_application.py
@@ -6,5 +6,3 @@ class TestApplicationModel:
         """Smoke tests for ApplicationCode enum."""
         app_enum = ApplicationCode
         assert app_enum.TEXT_TO_CODE.value == "text-to-code"
-        assert app_enum.ECR_REFINER.value == "ecr-refinement"
-        assert app_enum.QUERY_CONNECTOR.value == "additional-context-data"
