3ea33537b3e0d99cd248cd2fd234e810d1f1d370
Brady Fausett <brady.fausett@skylight.digital>
2026-01-30 09:08:22 -0700

    Update application.py

--- src/augmentation/models/application.py
+++ src/augmentation/models/application.py
@@ -5,5 +5,3 @@ class ApplicationCode(StrEnum):
     """The list of applications that will leveraging Augmentation functionality."""
 
     TEXT_TO_CODE = "text-to-code"
-    ECR_REFINER = "ecr-refinement"
-    QUERY_CONNECTOR = "additional-context-data"
