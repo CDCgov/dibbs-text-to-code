b02117af6c12b1105be83976dd907262fb5c9dff
Josh Nygaard <jnygaard@skylight.digital>
2026-02-04 11:14:11 -0500

    Moving everything under packages

---
+++ packages/text-to-code/tests/unit/test_schematron_processor.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+from text_to_code.models.eicr import DataField
+from text_to_code.services.schematron_processor import get_data_fields_from_schematron_error
+
+current_dir = Path(__file__).parent.parent
+
+
+class TestSchematronProcessor:
+    SCHEMATRON_ERROR_FILE = None
+
+    def file_setup(self) -> None:
+        if self.SCHEMATRON_ERROR_FILE is None:
+            schematron_path = current_dir / "assets" / "test_schematron_errors.xml"
+            with schematron_path.open() as f:
+                schematron_output = f.read()
+            self.SCHEMATRON_ERROR_FILE = schematron_output
+
+    def test_get_schematron_error_data_fields(self):
+        self.file_setup()
+        error_result = get_data_fields_from_schematron_error(
+            self.SCHEMATRON_ERROR_FILE,
+        )
+
+        expected_lab_test_name_resulted = 2
+        expected_lab_test_name_ordered = 1
+
+        assert (
+            len(error_result[DataField.LAB_TEST_NAME_RESULTED]) == expected_lab_test_name_resulted
+        )
+        assert len(error_result[DataField.LAB_TEST_NAME_ORDERED]) == expected_lab_test_name_ordered
+
+    def test_get_schematron_error_empty_xml(self):
+        schematron_errors = ""
+        result = get_data_fields_from_schematron_error(schematron_errors)
+
+        assert result == {}
--- tests/unit/augmentor/test_model_eicr.py
+++
@@ -1,9 +0,0 @@
-from src.augmentation.models.eicr import DataField
-
-
-class TestEICRModel:
-    def test_datafield_enum(self):
-        """Basic unit test for base (AugmenterConfig) config model."""
-        expected_size = 2
-        assert DataField.LAB_TEST_NAME_RESULTED.value == "Lab Test Name Resulted"
-        assert len(DataField) == expected_size
