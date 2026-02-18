1181a9becb220df04adfb5a7cf6914d69bdc8fc6
Josh Nygaard <jnygaard@skylight.digital>
2026-02-18 12:17:46 -0500

    add some removed tests

--- packages/augmentation/tests/unit/augmentor/test_augmenter.py
+++ packages/augmentation/tests/unit/augmentor/test_augmenter.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from uuid import UUID
 
 import pytest
+from augmentation.models.config import ApplicationCode
 from augmentation.models.config import AugmenterConfig
 from augmentation.models.config import TTCAugmenterConfig
 from augmentation.services.eicr_augmenter import EICRAugmenter
@@ -25,6 +26,18 @@ with aug_covid_ecr_path.open() as f:
 
 
 class TestEicrAugmenter:
+    def test_base_augmenter_with_no_document_data(self):
+        """Tests raising error when no document data is provided."""
+        with pytest.raises(ValueError, match=r"Document payload must be a non-empty string!"):
+            EICRAugmenter(document=None)
+
+    def test_ttc_augmenter_initialization(self):
+        """Tests initialization of the TTC augmenter."""
+        augmenter = EICRAugmenter(document=BASIC_ECR)
+        assert augmenter.application_code.value == ApplicationCode.TEXT_TO_CODE.value
+        assert augmenter.config == DATA_CONFIG
+        assert augmenter.original_xml == BASIC_ECR
+
     @pytest.mark.freeze_time("2026-02-13T15:27:57")
     def test_run(self, mocker: MockerFixture):
         """Tests augmentor run method."""
