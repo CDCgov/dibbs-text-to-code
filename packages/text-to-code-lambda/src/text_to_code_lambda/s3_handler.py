a8fad02bc847f5940612a569ac7e9fba789f38eb
Marcelle <53578688+m-goggins@users.noreply.github.com>
2026-02-24 12:58:09 -0800

    Update `s3_handler.py`  (#284)
    ## Description
    
    This PR updates our `s3_handler.py` file to include more of the
    functions that we know we'll need for deployment but are specific to
    AWS. These are functions for things like creating an OpenSearch client,
    checking that an S3 object exists, etc.
    
    ## Related Issues
    
    Closes #282 
    
    ## Additional Notes
    We'll likely need to add more functions are we get more details about
    the work with Geo/APHL but these are some things I know we'll need based
    on my tests in the Skylight/CDC AWS environment.
    
    Also updated the `lambda_function.py` filename, which is more consistent
    with the standard naming convention.

--- packages/text-to-code-lambda/src/text_to_code_lambda/s3_handler.py
+++ packages/text-to-code-lambda/src/text_to_code_lambda/s3_handler.py
@@ -4,21 +4,86 @@ import typing
 import boto3
 from aws_lambda_typing import events as lambda_events
 from botocore.client import BaseClient
+from botocore.exceptions import ClientError
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+
+
+def require_env(name: str) -> str:
+    """Fetch a required environment variable or raise a clear error.
+
+    :param name: The name of the environment variable to fetch.
+    :return: The value of the environment variable."""
+    value = os.getenv(name)
+    if not value:
+        raise ValueError(f"{name} not set as an environment variable.")
+    return value
+
+
+def strip_protocol(url: str) -> str:
+    """Remove http/https from a URL. This is sometimes needed for AWS service
+    endpoints (like OpenSearch) that require the URL without the protocol.
+    :param url: The URL to strip.
+    :return: The URL without the protocol."""
+    return url.removeprefix("https://").removeprefix("http://")
+
+
+def get_s3_credentials():
+    """Fetch AWS credentials from the environment """
+
+    return boto3.Session().get_credentials()
+
+
+def create_aws_auth() -> AWS4Auth:
+    """
+    Creates an AWS4Auth object for authenticating with AWS services.
+
+    :return: AWS4Auth object
+    """
+    credentials = get_s3_credentials()
+    return AWS4Auth(
+        credentials.access_key,
+        credentials.secret_key,
+        require_env("AWS_REGION"),
+        "es",
+        session_token=credentials.token,
+    )
 
 
 def create_s3_client() -> BaseClient:
     """
     Creates an S3 client.
+
+    :return: S3 client
     """
     endpoint_url = os.getenv("S3_ENDPOINT_URL")
-    region_name = os.getenv("AWS_REGION")
+    region_name = require_env("AWS_REGION")
 
     return boto3.client("s3", endpoint_url=endpoint_url, region_name=region_name)
+
+
+def create_opensearch_client(aws_auth: AWS4Auth) -> OpenSearch:
+    """
+    Creates an OpenSearch client.
+
+    :param aws_auth: AWS4Auth object for authentication
+    :return: OpenSearch client
+    """
+    endpoint_url = require_env("OPENSEARCH_ENDPOINT_URL")
+    return OpenSearch(
+        hosts=[{"host": strip_protocol(endpoint_url), "port": 443}],
+        http_auth=aws_auth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection,)
 
 
 def get_file_content_from_s3_event(event: lambda_events.EventBridgeEvent) -> bytes:
     """
     Extracts the file content from an S3 event triggered by a Lambda function.
+
+    :param event: The S3 event containing the bucket and object key information.
+    :return: The content of the file as bytes.
     """
 
     bucket_name = event["detail"]["bucket"]["name"]
@@ -33,6 +98,31 @@ def get_file_content_from_s3_event(event: lambda_events.EventBridgeEvent) -> byt
 def put_file(file_obj: typing.BinaryIO, bucket_name: str, object_key: str):
     """
     Uploads a file object to a S3 bucket.
+
+    :param file_obj: The file object to upload.
+    :param bucket_name: The name of the S3 bucket to upload to.
+    :param object_key: The key to assign to the uploaded object in S3.
     """
     client = create_s3_client()
     client.put_object(Body=file_obj, Bucket=bucket_name, Key=object_key)
+
+
+def check_s3_object_exists(s3_client: BaseClient, bucket: str, key: str) -> bool:
+    """Checks that an S3 object exists.
+
+    :param s3_client: The S3 client.
+    :param bucket: The name of the S3 bucket.
+    :param key: The key of the S3 object.
+    :raises Exception: If an unexpected error occurs while fetching the S3 object.
+    :return: True if the S3 object exists, False otherwise.
+    """
+    try:
+        s3_client.head_object(Bucket=bucket, Key=key)
+        return True
+    except ClientError as e:
+        error_code = e.response["Error"]["Code"]
+
+        if error_code in ("404", "NoSuchKey"):
+            return False
+
+        raise Exception(f"Unexpected error while fetching file from S3: {key}", e)
