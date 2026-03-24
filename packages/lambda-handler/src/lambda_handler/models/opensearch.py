66bc21059fd4f68d270885e200448334a9aeac80
Joshua Nygaard <jnygaard@skylight.digital>
2026-03-24 14:04:30 -0400

    from pydantic import  opensearch

--- packages/lambda-handler/src/lambda_handler/models/opensearch.py
+++ packages/lambda-handler/src/lambda_handler/models/opensearch.py
@@ -1,57 +1,50 @@
-import pydantic
+from pydantic import BaseModel
+from pydantic import Field
 
 from shared_models import OpenSearchHitSource
 
 
-class OpenSearchHit(pydantic.BaseModel):
+class OpenSearchHit(BaseModel):
     """Represents a single search result hit returned from OpenSearch."""
 
-    index: str = pydantic.Field(
+    index: str = Field(
         description="The index that the search result hit came from.", alias="_index"
     )
-    id: str = pydantic.Field(
-        description="The unique OpenSearch ID of the search result hit.", alias="_id"
-    )
-    score: float = pydantic.Field(
+    id: str = Field(description="The unique OpenSearch ID of the search result hit.", alias="_id")
+    score: float = Field(
         description="The cosine similarity score of the search result hit.", alias="_score"
     )
-    source: OpenSearchHitSource = pydantic.Field(
+    source: OpenSearchHitSource = Field(
         description="The source of the search result hit.", alias="_source"
     )
 
 
-class OpenSearchHits(pydantic.BaseModel):
+class OpenSearchHits(BaseModel):
     """Represents all of the search result hits returned from OpenSearch."""
 
-    total_hits: dict[str, int] = pydantic.Field(
+    total_hits: dict[str, int] = Field(
         alias="total", description="The total number of hits returned from OpenSearch."
     )
-    hits: list[OpenSearchHit] = pydantic.Field(
+    hits: list[OpenSearchHit] = Field(
         description="The list of search result hits returned from OpenSearch."
     )
 
 
-class OpenSearchShards(pydantic.BaseModel):
+class OpenSearchShards(BaseModel):
     """Represents the shard information returned from OpenSearch."""
 
-    total: int = pydantic.Field(description="The total number of shards involved in the search.")
-    successful: int = pydantic.Field(
-        description="The number of shards that successfully returned results."
-    )
-    skipped: int = pydantic.Field(
-        description="The number of shards that were skipped during the search."
-    )
-    failed: int = pydantic.Field(description="The number of shards that failed to return results.")
+    total: int = Field(description="The total number of shards involved in the search.")
+    successful: int = Field(description="The number of shards that successfully returned results.")
+    skipped: int = Field(description="The number of shards that were skipped during the search.")
+    failed: int = Field(description="The number of shards that failed to return results.")
 
 
-class OpenSearchResult(pydantic.BaseModel):
+class OpenSearchResult(BaseModel):
     """Represents the overall search result returned from OpenSearch, including hits and shard information."""
 
-    took: int = pydantic.Field(description="The time taken to execute the search in milliseconds.")
-    timed_out: bool = pydantic.Field(description="Indicates whether the search timed out.")
-    shards: OpenSearchShards = pydantic.Field(
+    took: int = Field(description="The time taken to execute the search in milliseconds.")
+    timed_out: bool = Field(description="Indicates whether the search timed out.")
+    shards: OpenSearchShards = Field(
         description="The shard information for the search.", alias="_shards"
     )
-    hits: OpenSearchHits = pydantic.Field(
-        description="The search result hits returned from OpenSearch."
-    )
+    hits: OpenSearchHits = Field(description="The search result hits returned from OpenSearch.")
