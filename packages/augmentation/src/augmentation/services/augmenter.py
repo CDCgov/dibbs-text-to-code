ce6f716d1f9fce0326999d1ab136a6abcab96e2e
Brady Fausett <brady.fausett@skylight.digital>
2026-03-24 14:24:41 -0600

    Update augmenter.py

--- packages/augmentation/src/augmentation/services/augmenter.py
+++ packages/augmentation/src/augmentation/services/augmenter.py
@@ -48,11 +48,10 @@ class Augmenter(ABC):
         return clean_xml_tree(v)
 
     def _get_application_code_value(self) -> str:
-        # adding checks here to resolve TYPE CHECK errors
-        return str(self.application_code.code)
+        return self.application_code.code
 
     def _get_application_code_display(self) -> str:
-        return str(self.application_code.display)
+        return self.application_code.display
 
     @abstractmethod
     def augment(self) -> Metadata:
