24ce8db706f0a020c7b702d74a36b49402a038b8
Joshua Nygaard <jnygaard@skylight.digital>
2026-04-22 11:19:11 -0400

    docstring

--- packages/utils/src/utils/utils.py
+++ packages/utils/src/utils/utils.py
@@ -4,14 +4,9 @@ from os import getenv
 def get_env_variable(name: str) -> str:
     """Grabs a variable by name from the environment. Throws an error if the variable is not present.
 
-    Args:
-        name (str): Name of the environment variable
-
-    Raises:
-        OSError: raised if environment variable is not present
-
-    Returns:
-        str: Name of the environment variable
+    :param name: Name of the environment variable
+    :raises OSError: raised if environment variable is not present
+    :return: Name of the environment variable
     """
     value = getenv(name)
     if value is None:
