1d8aef8001a5e3ff2eae5ea5bb9fee5b8e34bb0c
Joshua Nygaard <jnygaard@skylight.digital>
2026-04-29 15:28:45 -0400

    no errors test

--- packages/validation/tests/test_validation.py
+++ packages/validation/tests/test_validation.py
@@ -4,9 +4,7 @@ from validation import validate_eicr
 
 
 def test_validation():
-    with Path.open(
-        "/Users/jnygaard/Dev/Skylight/Dibbs/dibbs-text-to-code/e2e/assets/test_eicr.xml"
-    ) as f:
+    with Path.open("packages/validation/tests/assets/test_eicr.xml") as f:
         eicr = f.read()
     results = validate_eicr(eicr, True)
 
@@ -16,3 +14,11 @@ def test_validation():
             "location": "/Q{urn:hl7-org:v3}ClinicalDocument[1]/Q{urn:hl7-org:v3}component[1]/Q{urn:hl7-org:v3}structuredBody[1]/Q{urn:hl7-org:v3}component[1]/Q{urn:hl7-org:v3}section[1]/Q{urn:hl7-org:v3}entry[1]/Q{urn:hl7-org:v3}observation[1]",
         }
     ]
+
+
+def test_validation_no_errors():
+    with Path.open("e2e/snapshots/test_e2e/test_upload_and_process/augmented_eicr.xml") as f:
+        eicr = f.read()
+    results = validate_eicr(eicr, True)
+
+    assert results == []
