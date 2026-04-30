d6b2198622647872d3893d03d6f63a48e670fa76
Joshua Nygaard <jnygaard@skylight.digital>
2026-04-30 13:49:48 -0400

    add embedder benchmark

--- packages/text-to-code/tests/unit/test_embedder.py
+++ packages/text-to-code/tests/unit/test_embedder.py
@@ -7,8 +7,8 @@ class TestEmbedder:
     @pytest.mark.parametrize(
         ("input_text"), ["Influenza virus A and B and SARS-CoV-2 (COVID-19)", "COVID"]
     )
-    def test_embed(self, input_text: str) -> None:
-        embedding = embed(input_text)
+    def test_embed(self, input_text: str, benchmark) -> None:
+        embedding = benchmark(embed(input_text))
 
         expected_embedding_length = 1024
 
