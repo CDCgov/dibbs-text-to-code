36050bee5ed5a66760549fe865283d220ca99b9c
Brady Fausett <brady.fausett@skylight.digital>
2026-05-15 12:39:49 -0600

    Create functionality to extract LOINC Delta updates to generate additional embeddings (#532)
    ## Description
    
    This PR includes changes to make it possible to perform updates against,
    at least LOINC for now, various medical terminology value-sets utilized
    by TTC. Below is a list of the changes you will see:
    
    - Another refactor that moves the common functionality for All
    value-sets (general.py) as well as specific functionality for each of
    the different value-sets/ontologies into their own `util` python script.
    - The old valuset_sync script has been renamed to 'extraction' and
    leverages the utils functionality to perform a FULL extraction and saves
    the various files
    - The readme has been updated to indicate naming changes and location
    changes for various files/functionality
    - The embedding notebook was also changed to reflect the new order of
    all LOINC data pulled from LOINC extract so it will work correctly if
    ever used again.
    - **NOTE:** There are some formatting changes in this file for some
    reason and I'm not sure why - ignore these unless you notice an issue
    - Added a maintenance.py to handle maintaining the valueset extracts and
    embeddings for all valuesets - though only LOINC has been implemented so
    far for `loinc lab names`, as this is the only valueset loaded in our
    model at this point.
    
    ## Related Issues
    
    Closes #452 
    
    ## Additional Notes
    
    I haven't added in tests, but have tested along the way and have some
    print statements included for now just to see the end result. The output
    will be passed to the next step - create the embeddings - which will
    then be stored as files somewhere and then uploaded into OpenSearch.
    
    ## Checklist for Reviewers
    
    Please review and complete the following checklist during the review
    process:
    
    - [ ] The code follows best practices and conventions.
    - [ ] The changes implement the desired functionality or fix the
    reported issue.
    - [ ] The tests cover the new changes and pass successfully.
    - [ ] Any potential edge cases or error scenarios have been considered.

---
+++ packages/data-curation/src/data_curation/terminologies/snomed.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+"""
+data_curation.terminologies.utils.snomed
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This module contains a number of helper functions designed to assist
+with the process of extracting SNOMED codes and their terms 
+to generate and maintain embeddings in Opensearch for TTC.
+"""
+import requests
+from .general import clean_text_string, UMLS_API_KEY
+
+# Terminology URLS
+UMLS_SNOMED_LAB_VALUES_URL = (
+    "https://uts-ws.nlm.nih.gov/rest/content/current/source/SNOMEDCT_US/260245000/descendants"
+)
+
+
+def get_umls_snomed_lab_values() -> list[dict]:
+    """Process to get the all SNOMED Codes and terms for lab values
+        via the UMLS API.
+
+        :returns: A list of dictionaries containing SNOMED Lab Value records
+            including codes and text.
+    """
+    if UMLS_API_KEY is None:
+        raise KeyError("UMLS_API_KEY Environment Variable must be set to a proper UMLS API Key!")
+    page_num = 1
+    page_size = 500
+    params = {"apiKey": UMLS_API_KEY, "pageNumber": page_num, "pageSize": page_size}
+    umls_response = requests.get(UMLS_SNOMED_LAB_VALUES_URL, params=params)
+    snomed_row_count = 0
+    snomed_rows = []
+
+    while umls_response.status_code == 200:
+        # NOTE: the UMLS responses are a bit slow
+        #  you can use the print statement below to get a
+        #  better idea of the progress if needed.
+        # print(f"Processing SNOMED page {page_num}")
+        umls_results = umls_response.json().get("result")
+
+        for result in umls_results:
+            snomed_code = result.get("ui")
+            snomed_text = result.get("name")
+            if snomed_code and snomed_text:
+                result_row = {
+                    "code": snomed_code,
+                    "text": clean_text_string(snomed_text),
+                }
+                snomed_rows.append(result_row)
+                snomed_row_count += 1
+
+        page_num += 1
+        params = {"apiKey": UMLS_API_KEY, "pageNumber": page_num, "pageSize": page_size}
+        umls_response = requests.get(UMLS_SNOMED_LAB_VALUES_URL, params=params)
+        
+    # TODO: In Subsequent PR update this to be a logging statement
+    print(f"{snomed_row_count} Codes Extracted")
+    return snomed_rows
