6eae510a2ae090c763764b983947bc79f1269d7f
Joshua Nygaard <jnygaard@skylight.digital>
2026-05-22 15:21:29 -0400

    revert reranker

--- packages/text-to-code/src/text_to_code/services/reranker.py
+++ packages/text-to-code/src/text_to_code/services/reranker.py
@@ -1,15 +1,13 @@
-from pydantic import field_serializer
+from typing import TypedDict
+
 from sentence_transformers import CrossEncoder
 
-from shared_models import FrozenBaseModel
 from text_to_code.models.registry import TTC_RERANKER
 
 _RERANKER = CrossEncoder(TTC_RERANKER)
 
-_PRECISION = 3
 
-
-class ScoredResult(FrozenBaseModel):
+class ScoredResult(TypedDict):
     """The search result with its score."""
 
     code_string: str
@@ -17,11 +15,6 @@ class ScoredResult(FrozenBaseModel):
     This is the code's display name.
     """
     score: float
-
-    @field_serializer("score")
-    def round_floats(self, v: float) -> float:
-        """When serialized to JSON only save the first 3 digits."""
-        return round(v, _PRECISION)
 
 
 def rerank(nonstandard_in: str, hits: list[str]) -> list[ScoredResult]:
@@ -39,7 +32,6 @@ def rerank(nonstandard_in: str, hits: list[str]) -> list[ScoredResult]:
     """
     ranks = _RERANKER.rank(nonstandard_in, hits)
     sorted_ranks: list[ScoredResult] = [
-        ScoredResult(code_string=hits[r["corpus_id"]], score=round(r["score"], _PRECISION))
-        for r in ranks
+        {"code_string": hits[r["corpus_id"]], "score": r["score"]} for r in ranks
     ]
     return sorted_ranks
--- packages/text-to-code/tests/unit/test_reranker.py
+++ packages/text-to-code/tests/unit/test_reranker.py
@@ -1,6 +1,4 @@
-import pytest
-
-from text_to_code.services.reranker import rerank
+from text_to_code.services.reranker import ScoredResult, rerank
 
 
 class TestReranker:
@@ -13,8 +11,12 @@ class TestReranker:
             "Influenza virus A and B and SARS-CoV-2 (COVID-19)",
             ["Influenza virus A and B and SARS-CoV-2 (COVID-19)"],
         )
-        assert ranks[0].code_string == "Influenza virus A and B and SARS-CoV-2 (COVID-19)"
-        assert ranks[0].score == pytest.approx(0.989, abs=0.01)
+        ranks: list[ScoredResult] = [
+            {"code_string": r["code_string"], "score": round(float(r["score"]), 3)} for r in ranks
+        ]
+        assert ranks == [
+            {"code_string": "Influenza virus A and B and SARS-CoV-2 (COVID-19)", "score": 0.989}
+        ]
 
     def test_reranker_multiple_hits(self) -> None:
         nonstandard_in = "albumin/creatinine ratio (acr)"
@@ -25,11 +27,21 @@ class TestReranker:
             "Albumin/Creatinine (U) [Molar ratio]",
         ]
         ranks = rerank(nonstandard_in, search_hits)
-        assert ranks[0].code_string == "Albumin/Creatinine [Ratio] in Urine"
-        assert ranks[0].score == pytest.approx(0.541, abs=0.01)
-        assert ranks[1].code_string == "Albumin/Creatinine [Ratio] in 24 hour Urine"
-        assert ranks[1].score == pytest.approx(0.308, abs=0.01)
-        assert ranks[2].code_string == "Albumin/Creatinine (U) [Mass ratio]"
-        assert ranks[2].score == pytest.approx(0.21, abs=0.01)
-        assert ranks[3].code_string == "Albumin/Creatinine (U) [Molar ratio]"
-        assert ranks[3].score == pytest.approx(0.207, abs=0.01)
+        ranks: list[ScoredResult] = [
+            {"code_string": r["code_string"], "score": round(float(r["score"]), 3)} for r in ranks
+        ]
+        assert ranks == [
+            {
+                "code_string": "Albumin/Creatinine [Ratio] in Urine",
+                "score": 0.541,
+            },
+            {
+                "code_string": "Albumin/Creatinine [Ratio] in 24 hour Urine",
+                "score": 0.308,
+            },
+            {
+                "code_string": "Albumin/Creatinine (U) [Mass ratio]",
+                "score": 0.21,
+            },
+            {"code_string": "Albumin/Creatinine (U) [Molar ratio]", "score": 0.207},
+        ]
