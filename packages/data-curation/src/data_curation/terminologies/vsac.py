40ada2a56dd44f20064d7f2511a0e2e8564ec11a
Joshua Nygaard <jnygaard@skylight.digital>
2026-05-26 15:37:06 -0400

    resolve issues in vsac

--- packages/data-curation/src/data_curation/terminologies/vsac.py
+++ packages/data-curation/src/data_curation/terminologies/vsac.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 """data_curation.terminologies.utils.vsac.
-~~~~~~~~~~~~~~~~~~~~~~~~~
 
 This module contains a number of helper functions designed to assist
 with the process of extracting The Value Set Authority Center (VSAC)
@@ -19,12 +18,11 @@ VSAC_PROBLEMS_URL = (
     "https://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.88.12.3221.7.4/$expand"
 )
 
+_TIMEOUT = 61
+
 
 def get_vsac_rxnorm_medications() -> list[dict]:
-    """Function to get the all VSAC RXNorm Codes and terms for medications
-    via the NLM/NIH VSAC API. There is an underlying function that
-    processes the results from various VSAC API results into a
-    common structure that is leveraged within this function.
+    """Function to get the all VSAC RXNorm Codes and terms for medications via the NLM/NIH VSAC API. There is an underlying function that processes the results from various VSAC API results into a common structure that is leveraged within this function.
 
     :returns: A list of dictionaries containing RXNorm Medication records
         including codes and text.
@@ -33,10 +31,7 @@ def get_vsac_rxnorm_medications() -> list[dict]:
 
 
 def get_vsac_cvx_vaccines() -> list[dict]:
-    """Function to get the all VSAC CVX Codes and terms for administered
-    vaccines via the NLM/NIH VSAC API.  There is an underlying
-    function that processes the results from various VSAC API
-    results into a common structure that is leveraged within this function.
+    """Function to get the all VSAC CVX Codes and terms for administered vaccines via the NLM/NIH VSAC API.  There is an underlying function that processes the results from various VSAC API results into a common structure that is leveraged within this function.
 
     :returns: A list of dictionaries containing RXNorm Medication records
         including codes and text.
@@ -46,11 +41,7 @@ def get_vsac_cvx_vaccines() -> list[dict]:
 
 # problems are also known as "Diagnosis/Symptom Codes"
 def get_vsac_snomed_problems() -> list[dict]:
-    """Function to get the all VSAC SNOMED Codes and terms for
-    Diagnosis/Problems via the NLM/NIH VSAC API. There is an
-    underlying function that processes the results from various
-    VSAC API results into a common structure that is leveraged
-    within this function.
+    """Function to get the all VSAC SNOMED Codes and terms for Diagnosis/Problems via the NLM/NIH VSAC API. There is an underlying function that processes the results from various VSAC API results into a common structure that is leveraged within this function.
 
     :returns: A list of dictionaries containing SNOMED Problem records
         including codes and text.
@@ -59,9 +50,7 @@ def get_vsac_snomed_problems() -> list[dict]:
 
 
 def process_vsac_codes(api_url: str, vs_type: str) -> list[dict]:
-    """Process to obtain VSAC API results for various value sets
-    and organize them into a common structure that is leveraged
-    by other functions within this module.
+    """Process to obtain VSAC API results for various value sets and organize them into a common structure that is leveraged by other functions within this module.
 
     :returns: A list of dictionaries containing different value set records
         including codes and text.
@@ -71,12 +60,14 @@ def process_vsac_codes(api_url: str, vs_type: str) -> list[dict]:
 
     record_offset = 0
     params = {"offset": record_offset}
-    vsac_response = requests.get(api_url, params=params, auth=("apikey", UMLS_API_KEY))
+    vsac_response = requests.get(
+        api_url, params=params, auth=("apikey", UMLS_API_KEY), timeout=_TIMEOUT
+    )
     record_count = 0
     total_records = 1
     data_rows = []
 
-    while vsac_response.status_code == 200 and record_count < total_records:
+    while vsac_response.status_code == requests.codes.ok and record_count < total_records:
         # get the offset and record counts from the 'expansion'
         vsac_expansion = vsac_response.json().get("expansion")
         if vsac_expansion:
@@ -104,6 +95,8 @@ def process_vsac_codes(api_url: str, vs_type: str) -> list[dict]:
 
         if total_records != record_count:
             params = {"offset": record_count}
-            vsac_response = requests.get(api_url, params=params, auth=("apikey", UMLS_API_KEY))
+            vsac_response = requests.get(
+                api_url, params=params, auth=("apikey", UMLS_API_KEY), timeout=_TIMEOUT
+            )
     # TODO: In Subsequent PR update this to be a logging statement
     return data_rows
