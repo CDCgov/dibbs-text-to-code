65d6afba2ff5923b14105ce8ff0877af1760d118
Josh Nygaard <141273852+JNygaard-Skylight@users.noreply.github.com>
2026-06-11 12:39:33 -0400

    refactor: ruff data curation (#607)
    ## Description
    
    - Resolves ruff issues in the data curation package.
    - Deletes the archived directory in data curation.
    - Moves data emulation code into `data_emulation` package.
    
    ## Related Issues
    
    Closes #572

--- packages/data-curation/src/data_curation/terminologies/snomed.py
+++ packages/data-curation/src/data_curation/terminologies/snomed.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
 
-"""
-data_curation.terminologies.utils.snomed
-~~~~~~~~~~~~~~~~~~~~~~~~~
+"""This module contains a number of helper functions designed to assis with the process of extracting SNOMED codes and their term to generate and maintain embeddings in Opensearch for TTC."""
 
-This module contains a number of helper functions designed to assist
-with the process of extracting SNOMED codes and their terms 
-to generate and maintain embeddings in Opensearch for TTC.
-"""
-import requests
-from .general import clean_text_string, UMLS_API_KEY
+from .general import UMLS_API_KEY, clean_text_string
+from .http_client import STATUS_CODE_OK, get_with_timeout
 
 # Terminology URLS
 UMLS_SNOMED_LAB_VALUES_URL = (
@@ -18,22 +12,21 @@ UMLS_SNOMED_LAB_VALUES_URL = (
 
 
 def get_umls_snomed_lab_values() -> list[dict]:
-    """Process to get the all SNOMED Codes and terms for lab values
-        via the UMLS API.
+    """Process to get the all SNOMED Codes and terms for lab values via the UMLS API.
 
-        :returns: A list of dictionaries containing SNOMED Lab Value records
-            including codes and text.
+    :returns: A list of dictionaries containing SNOMED Lab Value records
+        including codes and text.
     """
     if UMLS_API_KEY is None:
         raise KeyError("UMLS_API_KEY Environment Variable must be set to a proper UMLS API Key!")
     page_num = 1
     page_size = 500
     params = {"apiKey": UMLS_API_KEY, "pageNumber": page_num, "pageSize": page_size}
-    umls_response = requests.get(UMLS_SNOMED_LAB_VALUES_URL, params=params)
+    umls_response = get_with_timeout(UMLS_SNOMED_LAB_VALUES_URL, params=params)
     snomed_row_count = 0
     snomed_rows = []
 
-    while umls_response.status_code == 200:
+    while umls_response.status_code == STATUS_CODE_OK:
         # NOTE: the UMLS responses are a bit slow
         #  you can use the print statement below to get a
         #  better idea of the progress if needed.
@@ -53,8 +46,8 @@ def get_umls_snomed_lab_values() -> list[dict]:
 
         page_num += 1
         params = {"apiKey": UMLS_API_KEY, "pageNumber": page_num, "pageSize": page_size}
-        umls_response = requests.get(UMLS_SNOMED_LAB_VALUES_URL, params=params)
-        
+        umls_response = get_with_timeout(UMLS_SNOMED_LAB_VALUES_URL, params=params)
+
     # TODO: In Subsequent PR update this to be a logging statement
     print(f"{snomed_row_count} Codes Extracted")
     return snomed_rows
