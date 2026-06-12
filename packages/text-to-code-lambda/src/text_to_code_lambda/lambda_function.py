94fdbe2c431eebbdd50fdcf83da1b7902c90eb0a
m-goggins <marcelle.goggins@gmail.com>
2026-06-12 12:47:21 -0400

    fix typo to get both reranker and retriever

--- packages/text-to-code-lambda/src/text_to_code_lambda/lambda_function.py
+++ packages/text-to-code-lambda/src/text_to_code_lambda/lambda_function.py
@@ -12,7 +12,7 @@ from shared_models import Code, NonstandardCodeInstance, PassthroughReason, TTCA
 from text_to_code.models import query as query_models
 from text_to_code.models.model_info import TTCModelInfo
 from text_to_code.services import eicr_processor, evaluator, schematron_processor
-from text_to_code.services.embedder import embed
+from text_to_code.services.embedder import _RETRIEVER_MODEL_INFO, embed
 from text_to_code.services.query import QueryBuilder
 from text_to_code.services.reranker import _RERANKER_MODEL_INFO, ScoredResult, rerank
 
@@ -128,7 +128,7 @@ def _write_ttc_exception_passthrough_output(record: SQSRecord, error: Exception)
             error=str(error),
             model_info=TTCModelInfo(
                 reranker=_RERANKER_MODEL_INFO,
-                retriever=_RERANKER_MODEL_INFO,
+                retriever=_RETRIEVER_MODEL_INFO,
             ),
         )
         ttc_output = TTCAugmenterInput(
@@ -450,7 +450,7 @@ def _process_record_pipeline(
         passthrough=passthrough_reason is not None,
         passthrough_reason=passthrough_reason,
         model_info=TTCModelInfo(
-            retriever=_RERANKER_MODEL_INFO,
+            retriever=_RETRIEVER_MODEL_INFO,
             reranker=_RERANKER_MODEL_INFO,
         ),
     )
