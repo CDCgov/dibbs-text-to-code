a265dca391bd8cc281df4327598de57e4e1ba60b
bamader <49412165+bamader@users.noreply.github.com>
2026-07-01 14:27:40 -0400

    feat: Add result cache to lambda loop (#589)
    ## Description
    
    This PR adds the final integration of the result cache service into the
    per-invocation loop of the TTC lambda. CloudWatch metrics are also
    emitted as part of the loop, indicating whether the result cache was a
    hit or a miss.
    
    ## Related Issues
    
    Closes #533 
    
    ## Additional Notes
    
    The current implementation uses a lot of object creation to build
    properly typed data bundles and preserve our access patterns established
    elsewhere in the code. I don't love how this looks in most places (e.g.
    the result-cache `GET` API service, a lot of the tests, etc.) because
    there's some repeated, clunky-looking instantiation calls, but I went
    with this structure for two main reasons:
    
    1. it preserves the way we access objects in other parts of the code
    base. For example, now both branches of the lambda invocation can treat
    variables the same, since whether or not the result-cache gets used, all
    objects and data fields have the expected information and expected types
    in them.
    2. it mirrors the way other API responses are structured. For example,
    in `lambda_handler.py` > `retrieve_opensearch_results`, we go through
    the effort of turning the JSON response dictionary into a set of objects
    with expected typing and data structures. I assume this is because the
    OpenSearch GET response is actually quite dense (it's linked in the
    result cache function docs, but there are a lot of possible return
    fields), so explicitly defining this preserves logic for the rest of the
    code base. I adopted the same mentality here.
    
    I'm not married to this class-based implementation, but I do think it's
    consistent with the rest of our code, so just wanted to outline the
    decision there.
    
    ---------
    
    Co-authored-by: Rob Mitchell <robert.a.mitchell@skylight.digital>
    Co-authored-by: Joshua Nygaard <jnygaard@skylight.digital>
    Co-authored-by: Josh Nygaard <141273852+JNygaard-Skylight@users.noreply.github.com>
    Co-authored-by: Nick Clyde <nick@clyde.tech>

--- packages/text-to-code/tests/unit/test_result_cache.py
+++ packages/text-to-code/tests/unit/test_result_cache.py
@@ -1,7 +1,12 @@
-import json
-from typing import cast
 from unittest.mock import MagicMock
 
+from lambda_handler.models import (
+    OpenSearchHit,
+    OpenSearchHits,
+    OpenSearchHitSource,
+    OpenSearchResult,
+    OpenSearchShards,
+)
 from shared_models import Code
 from text_to_code.services.result_cache import get_cached_result, put_new_cached_result
 
@@ -20,19 +25,29 @@ class TestResultCacheAPIs:
             "primary_term": "3",
             "found": True,
             "routing": "",
-            "source": {
+            "_source": {
                 "cache_key": "1357924680",
                 "text": "Screening urine fentanyl detection",
                 "data_field": "Lab Test Name Ordered",
-                "loinc_code": json.dumps(
-                    Code(
-                        code_system="2.16.840.1.113883.6.1",
-                        code_system_name="LOINC",
-                        display_name="fentaNYL [Presence] in Urine by Screen method",
-                    ).__dict__
-                ),
+                "loinc_code": {
+                    "code": "51459-2",
+                    "code_system": "2.16.840.1.113883.6.1",
+                    "code_system_name": "LOINC",
+                    "display_name": "fentaNYL [Presence] in Urine by Screen method",
+                    "original_text": "Screening urine fentanyl detection",
+                },
                 "search_score": 0.9563,
                 "reranker_score": 0.6789,
+                "opensearch_retrieved_scores": {
+                    "took": 234,
+                    "timed_out": False,
+                    "_shards": {"total": 1, "successful": 1, "failed": 0, "skipped": 0},
+                    "hits": {
+                        "total": {},
+                        "hits": [],
+                    },
+                },
+                "reranker_processed_results": {"results": []},
                 "cached_at": "2026-05-15T18:14:45.020655+00:00",
             },
             "fields": {},
@@ -43,9 +58,8 @@ class TestResultCacheAPIs:
         )
 
         assert cached_result is not None
-        cached_result_source = cast(dict[str, object], cached_result)
-        assert cached_result_source["cache_key"] == "1357924680"
-        assert cached_result_source["text"] == "Screening urine fentanyl detection"
+        assert cached_result.cache_key == "1357924680"
+        assert cached_result.text == "Screening urine fentanyl detection"
 
     def test_get_miss(self):
         """Tests the Result Cache's GET functionality when the document is absent."""
@@ -58,7 +72,7 @@ class TestResultCacheAPIs:
             "primary_term": "",
             "found": False,
             "routing": "",
-            "source": {},
+            "_source": {},
             "fields": {},
         }
 
@@ -85,6 +99,28 @@ class TestResultCacheAPIs:
             display_name="Urea nitrogen [Mass/volume] in Blood",
         )
 
+        opensearch_shards = OpenSearchShards(total=1, successful=1, failed=0, skipped=0)
+        opensearch_hit_source = OpenSearchHitSource(
+            id=139924673,
+            loinc_code="6299-2",
+            loinc_name_type="",
+            description="Urea nitrogen [Mass/volume] in Blood",
+            loinc_type="BOTH",
+        )
+        opensearch_hit = OpenSearchHit(
+            _index=RESULT_CACHE_INDEX_NAME,
+            _id="139924673",
+            _score=0.97771,
+            _source=opensearch_hit_source,
+        )
+        opensearch_hits = OpenSearchHits(total={"hit": 1}, hits=[opensearch_hit])
+        opensearch_retrieved_scores = OpenSearchResult(
+            took=234,
+            timed_out=False,
+            _shards=opensearch_shards,
+            hits=opensearch_hits,
+        )
+
         cache_result_created = put_new_cached_result(
             mock_opensearch_client,
             RESULT_CACHE_INDEX_NAME,
@@ -93,6 +129,8 @@ class TestResultCacheAPIs:
             standard_loinc_code,
             0.97771,
             0.8624,
+            opensearch_retrieved_scores,
+            [],
         )
 
         assert cache_result_created
