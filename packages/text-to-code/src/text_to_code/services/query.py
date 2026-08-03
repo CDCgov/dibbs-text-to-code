5b8ed5a698b86e5fd6255f73c5ce3845e6ddbe7e
Joshua Nygaard <jnygaard@skylight.digital>
2026-08-03 14:30:40 -0400

    remove exclude vector field

--- packages/text-to-code/src/text_to_code/services/query.py
+++ packages/text-to-code/src/text_to_code/services/query.py
@@ -96,9 +96,6 @@ class QueryBuilder:
         return {
             "size": self._size,
             "_source": {
-                "excludes": [
-                    self._vector_field
-                ],  # Exclude the vector field from the results to reduce payload size & improve performance
                 "includes": ["id", "loinc_code", "loinc_name_type", "description", "loinc_type"],
             },
             "query": {
